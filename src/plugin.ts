748230fca46770ea0c031ec2c430f2620c38123b
Wes Higbee <wes.mcclure@gmail.com>
2025-11-11 04:33:05 -0600

    use streamdeck's logger

--- src/plugin.ts
+++ src/plugin.ts
@@ -8,7 +8,15 @@ streamDeck.connect();
 import { startExternalServer as startExternalWebSocketServer } from './webby';
 
 streamDeck.system.onDidReceiveDeepLink((ev) => {
+    // TODO! use passive links to send messages instead of using a websocket server?
+    // sounds good to me! if it works ok and doesn't open the streamdeck app
+    // not clunky slow too, right?
+
     console.log('deeplink', ev);
+
+    // TODO! switch to using logger in SDeck
+    //  any limitations?
+    streamDeck.logger.info('deeper', ev); // shows up in com.wes.kmtrigger.sdPlugin/logs/*
 
     // https://docs.elgato.com/streamdeck/sdk/guides/deep-linking
     //
