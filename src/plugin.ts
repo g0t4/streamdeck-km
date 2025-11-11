6c9c7093c8e5f225e17a6f5dc76cf21892643267
Wes Higbee <wes.mcclure@gmail.com>
2025-11-11 04:27:12 -0600

    deep link example

--- src/plugin.ts
+++ src/plugin.ts
@@ -6,4 +6,32 @@ streamDeck.actions.registerAction(new TriggerMacro());
 streamDeck.connect();
 
 import { startExternalServer as startExternalWebSocketServer } from './webby';
+
+streamDeck.system.onDidReceiveDeepLink((ev) => {
+    console.log('deeplink', ev);
+
+    // https://docs.elgato.com/streamdeck/sdk/guides/deep-linking
+    //
+    // active:
+    //   streamdeck://plugins/message/com.wes.kmtrigger/hello?name=Elgato#waving
+    //
+    // passive:
+    //   streamdeck://plugins/message/com.wes.kmtrigger/hello?streamdeck=hidden
+    //    v7 has passive deep link (streamdeck=hidden) but it's not working for me
+    //    these won't open streamdeck app (like active links)
+    //
+    //  ev: {
+    //     "type": "didReceiveDeepLink",
+    //     "url": {
+    //         "fragment": "waving",
+    //         "href": "/hello?name=Elgato#waving",
+    //         "path": "/hello",
+    //         "query": "name=Elgato",
+    //         "queryParameters": {}
+    //     }
+    // }  
+    //
+
+});
+
 startExternalWebSocketServer();
