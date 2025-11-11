1dfd04fda3559710892baeb2d873e879f9d4e697
Wes Higbee <wes.mcclure@gmail.com>
2025-11-10 22:25:07 -0600

    format

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,38 +12,44 @@ const sdPlugin = "com.wes.kmtrigger.sdPlugin";
  * @type {import('rollup').RollupOptions}
  */
 const config = {
-	input: "src/plugin.ts",
-	output: {
-		file: `${sdPlugin}/bin/plugin.js`,
-		sourcemap: isWatching,
-		sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
-			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href;
-		}
-	},
-	plugins: [
-		{
-			name: "watch-externals",
-			buildStart: function () {
-				this.addWatchFile(`${sdPlugin}/manifest.json`);
-			},
-		},
-		typescript({
-			mapRoot: isWatching ? "./" : undefined
-		}),
-		nodeResolve({
-			browser: false,
-			exportConditions: ["node"],
-			preferBuiltins: true
-		}),
-		commonjs(),
-		!isWatching && terser(),
-		{
-			name: "emit-module-package-file",
-			generateBundle() {
-				this.emitFile({ fileName: "package.json", source: `{ "type": "module" }`, type: "asset" });
-			}
-		}
-	]
+    input: "src/plugin.ts",
+    output: {
+        file: `${sdPlugin}/bin/plugin.js`,
+        sourcemap: isWatching,
+        sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+            return url.pathToFileURL(
+                path.resolve(path.dirname(sourcemapPath), relativeSourcePath),
+            ).href;
+        },
+    },
+    plugins: [
+        {
+            name: "watch-externals",
+            buildStart: function () {
+                this.addWatchFile(`${sdPlugin}/manifest.json`);
+            },
+        },
+        typescript({
+            mapRoot: isWatching ? "./" : undefined,
+        }),
+        nodeResolve({
+            browser: false,
+            exportConditions: ["node"],
+            preferBuiltins: true,
+        }),
+        commonjs(),
+        !isWatching && terser(),
+        {
+            name: "emit-module-package-file",
+            generateBundle() {
+                this.emitFile({
+                    fileName: "package.json",
+                    source: `{ "type": "module" }`,
+                    type: "asset",
+                });
+            },
+        },
+    ],
 };
 
 export default config;
